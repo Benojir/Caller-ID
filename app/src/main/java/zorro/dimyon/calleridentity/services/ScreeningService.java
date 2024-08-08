6d2bcfa58a217229e0a4aefd42b218c83387e4ce
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2024-08-08 15:39:40 +0530

    modified

--- app/src/main/java/zorro/dimyon/calleridentity/services/ScreeningService.java
+++ app/src/main/java/zorro/dimyon/calleridentity/services/ScreeningService.java
@@ -56,43 +56,45 @@ public class ScreeningService extends CallScreeningService {
                 String countryNameCode = loginSaver.getCountryNameCode();
                 String apiKey = loginSaver.getApiKey();
 
-                GetPhoneNumberInfo callerInfo = new GetPhoneNumberInfo(this, number, countryNameCode, apiKey);
-                callerInfo.getNumberInfo(new GetPhoneNumberInfo.OnFetchedInfoListener() {
-                    @Override
-                    public void onSuccess(JSONObject numberInfo) {
+                if (!apiKey.isEmpty()) {
+                    GetPhoneNumberInfo callerInfo = new GetPhoneNumberInfo(this, number, countryNameCode, apiKey);
+                    callerInfo.getNumberInfo(new GetPhoneNumberInfo.OnFetchedInfoListener() {
+                        @Override
+                        public void onSuccess(JSONObject numberInfo) {
 
-                        try {
-                            String callerName = numberInfo.getString("callerName");
-                            String address = numberInfo.getString("address");
-                            boolean isSpamCall = numberInfo.getBoolean("isSpamCall");
-                            String spamType = numberInfo.getString("spamType");
+                            try {
+                                String callerName = numberInfo.getString("callerName");
+                                String address = numberInfo.getString("address");
+                                boolean isSpamCall = numberInfo.getBoolean("isSpamCall");
+                                String spamType = numberInfo.getString("spamType");
 
-                            if (isSpamCall) {
-                                response.setDisallowCall(true);
-                                response.setRejectCall(true);
-                                respondToCall(callDetails, response.build());
+                                if (isSpamCall) {
+                                    response.setDisallowCall(true);
+                                    response.setRejectCall(true);
+                                    respondToCall(callDetails, response.build());
 
-                                Toast.makeText(ScreeningService.this, "Spam call blocked.", Toast.LENGTH_SHORT).show();
-                            } else {
-                                // Start the PopupService
-                                Intent intent = new Intent(ScreeningService.this, PopupService.class);
-                                intent.putExtra("caller_name", callerName);
-                                intent.putExtra("phone_number", number);
-                                intent.putExtra("address", address);
-                                intent.putExtra("spam_type", spamType);
-                                startForegroundService(intent);
-                                Log.d(TAG, "onScreenCall: " + number);
+                                    Toast.makeText(ScreeningService.this, "Spam call blocked.", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // Start the PopupService
+                                    Intent intent = new Intent(ScreeningService.this, PopupService.class);
+                                    intent.putExtra("caller_name", callerName);
+                                    intent.putExtra("phone_number", number);
+                                    intent.putExtra("address", address);
+                                    intent.putExtra("spam_type", spamType);
+                                    startForegroundService(intent);
+                                    Log.d(TAG, "onScreenCall: " + number);
+                                }
+                            } catch (Exception e) {
+                                Log.e(TAG, "onSuccess: ", e);
                             }
-                        } catch (Exception e) {
-                            Log.e(TAG, "onSuccess: ", e);
                         }
-                    }
 
-                    @Override
-                    public void onError(String errorMessage) {
-                        Log.d(TAG, "onError: " + errorMessage);
-                    }
-                });
+                        @Override
+                        public void onError(String errorMessage) {
+                            Log.d(TAG, "onError: " + errorMessage);
+                        }
+                    });
+                }
             }
         }
     }
